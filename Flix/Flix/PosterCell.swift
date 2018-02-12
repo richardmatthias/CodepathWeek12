9c684a9cfca4a39fc59fe8e4774a4235406d8ce1
richardmatthias <richardmatthias@126.com>
2018-02-11 16:10:30 -0800

    Assignment 2 Revised

---
+++ Flix/Flix/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Langtian Qin on 2/11/18.
+//  Copyright © 2018 Langtian Qin. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
